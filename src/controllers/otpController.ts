21c338bf77c4222792eecc9e41a4db2b65bd3cea
Trushka99 <trushka12512@yandex.ru>
2026-06-29 13:01:12 +0300

    vcnd

--- src/controllers/otpController.ts
+++ src/controllers/otpController.ts
@@ -69,10 +69,10 @@ export const sendOtpService = async (req: Request, res: Response) => {
       expiresAt: new Date(Date.now() + 5 * 60 * 1000),
       maxAttempts: 2,
     });
-    // const coderes = await sendSmsCode(phone, code, type);
+    const coderes = await sendSmsCode(phone, code, type);
     return sendSuccess(
       res,
-      { code: code },
+      { code: code, res: coderes },
       ErrorCodes.OTP_SENT_SUCCESS,
       200,
     );
